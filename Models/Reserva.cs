87b64e2a4deac1065c3beff8a14ed36f27f6d89d
Davyson de Azevedo <158609502+Azevd0@users.noreply.github.com>
2025-09-24 13:20:26 -0300

    Update Reserva.cs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,16 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+                if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
+                Console.WriteLine("Hóspedes cadastrados com sucesso.");
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("A capacidade é menor que o número de hóspedes.");
             }
         }
 
@@ -37,6 +39,10 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
             return 0;
         }
 
@@ -45,16 +51,18 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+             decimal valorTotal = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+             decimal desconto = valorTotal * 0.10M;
+             valorTotal -= desconto;
+                Console.WriteLine($"Desconto de 10% aplicado: R$ {desconto}");
             }
 
-            return valor;
+            return valorTotal;
         }
     }
 }
